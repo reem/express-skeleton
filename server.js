90ccb0e2512de47904b3efcc4ac06dafd8f7c37a
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-16 14:02:53 -0700

    Added basic express server.

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var _ = require('lodash/dist/underscore');
+
+var app = express();
+
+app.use(morgan());
+app.use(bodyParser());
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000);
+console.log("On 3k");
