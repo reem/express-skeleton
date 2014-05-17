216d7af13a99ad8e4409222fa4e009e194b8cdaf
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-16 21:01:18 -0700

    Added routes pattern.

--- server.js
+++ server.js
@@ -10,5 +10,7 @@ app.use(bodyParser());
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
+require('./routes.js')(app);
+
 app.listen(3000);
 console.log("On 3k");
